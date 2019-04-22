a6917e9d49948f08d03194ba057c44978e9d0f8c
Vladislav <vladislav5640@gmail.com>
2019-04-22 08:04:01 +0300

    user name renaming

--- src/main/kotlin/ua/nure/nosql/notes/entity/User.kt
+++ src/main/kotlin/ua/nure/nosql/notes/entity/User.kt
@@ -3,4 +3,4 @@ package ua.nure.nosql.notes.entity
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 
-data class User (@Id var id: ObjectId, var name: String, var password: String)
+data class User (@Id var id: ObjectId, var userName: String, var password: String)
