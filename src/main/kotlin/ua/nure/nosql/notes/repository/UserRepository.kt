f91189dbb6c7ed12bf725c7118c0ed9da77dc963
Vladislav <vladislav5640@gmail.com>
2019-04-23 07:08:20 +0300

    return optional types in repositories

--- src/main/kotlin/ua/nure/nosql/notes/repository/UserRepository.kt
+++ src/main/kotlin/ua/nure/nosql/notes/repository/UserRepository.kt
@@ -3,12 +3,13 @@ package ua.nure.nosql.notes.repository
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 import ua.nure.nosql.notes.entity.User
+import java.util.*
 
 interface UserRepository : MongoRepository<User, ObjectId> {
 
     fun existsUserByUserName(userName: String): Boolean
 
-    fun findUserByUserName(name: String): User?
+    fun findUserByUserName(name: String): Optional<User>
 
-    fun findUserByUserNameAndPassword(name: String, password: String): User?
+    fun findUserByUserNameAndPassword(name: String, password: String): Optional<User>
 }
